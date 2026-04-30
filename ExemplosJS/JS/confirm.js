47379213d673dc1e593ba610f78026d338cb3a44
Pedro Comis <comispedro95@gmail.com>
2026-04-30 11:45:37 -0300

    atividade15-atualizacao

--- ExemplosJS/JS/confirm.js
+++ ExemplosJS/JS/confirm.js
@@ -11,3 +11,6 @@ function excluir(){
 function abrir(){
     return window.confirm('Você tem certeza que deseja sair dessa página e abrir uma nova página?')
 }
+function carrinho(){
+    return window.confirm('Voocê tem certeza que deseja adicionar este item ao carrinho?')
+}
