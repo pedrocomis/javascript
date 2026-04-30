b2eea0dd9fc8c0758b62643256609069bad81f0b
Pedro Comis <comispedro95@gmail.com>
2026-04-30 11:07:43 -0300

    novos arquivos criados de exemplo

---
+++ ExemplosJS/JS/confirm.js
@@ -0,0 +1,7 @@
+function confirmar(){
+   return window.confirm('Você tem certeza que deseja confirmar?')
+}
+
+function navConfirm(){
+    return window.confirm('deseja realmente ir para essa página?')
+}
