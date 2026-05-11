fabff13c456953fb3412f98e638fc421842aa5b1
Pedro Comis <comispedro95@gmail.com>
2026-05-11 11:05:49 -0300

    atividade-20 atualizaçao

--- Atividades/js/atividades.js
+++ Atividades/js/atividades.js
@@ -1,3 +1,6 @@
 function carrinho(){
     return window.confirm('Você tem certeza que deseja adicionar este item para seu carrinho?')
 }
+function login(){
+    return window.confirm('Você tem certeza que deseja fazer login?')
+}
